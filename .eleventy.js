0fe42b7a28593b443ea5f9a17e66b134c9e578d6
patrickkasl <patrickkasl91@gmail.com>
2021-08-20 23:03:09 +0200

    Animated eyes and cursor

--- .eleventy.js
+++ .eleventy.js
@@ -34,6 +34,9 @@ module.exports = function (eleventyConfig) {
       "./static/css/prism-tomorrow.css",
   });
 
+  // Copy JS Folder to /_site
+  eleventyConfig.addPassthroughCopy("./src/static/js");
+
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
 
