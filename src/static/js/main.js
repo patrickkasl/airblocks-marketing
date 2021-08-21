427269be726a519ea9e0d67e2abd56e8fb63a437
patrickkasl <patrickkasl91@gmail.com>
2021-08-21 13:00:37 +0200

    favicon + about page for desktop

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -5,7 +5,7 @@ $("#about").mousemove(function(event) {
     var x = (eye.offset().left) + (eye.width() / 2);
     var y = (eye.offset().top) + (eye.height() / 2);
     var rad = Math.atan2(event.pageX - x, event.pageY - y);
-    var rot = (rad * (180 / Math.PI) * -1) + 180;
+    var rot = (rad * (180 / Math.PI) * -1) + 130;
     eye.css({
       '-webkit-transform': 'rotate(' + rot + 'deg)',
       '-moz-transform': 'rotate(' + rot + 'deg)',
